7841e5b05c8067bdf8ae210771ac4c0b3ed3c5fc
Ian Mike <iammikeian@gmail.com>
2023-07-25 13:58:26 +0300

    Use malloc to allocate size

--- print_character.c
+++ print_character.c
@@ -1,15 +1,25 @@
 #include "main.h"
 
 /**
- * print_char - print characters 
+ * print_char - print characters
  * @list: number of parameters
  * Return: success
  */
 
 int print_char(va_list list)
 {
-	char c;
+	char *c;
+	int len;
 
-	c = va_arg(list, int);
-	return (write(1, &c ,1);
+	c = (char *)malloc(sizeof(char));
+	if (c)
+	{
+		*c = va_arg(list, int);
+		len = write(1, c, 1);
+		free(c);
+
+		return (len);
+	}
+	else
+		return (-1);
 }
