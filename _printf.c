a1f3261ecc82a4347512ec177602603491d3f405
The Taxman <108511481+init-commit@users.noreply.github.com>
2023-07-25 10:36:11 +0300

    :sparkles: A function that produces output according to a format handler

---
+++ _printf.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* _printf - produces output according to a format
+* @format: character
+* Return: number of characters
+*/
+int _printf(const char *format, ...)
+{
+  va_list list;
+	int len = 0;
+
+	va_start(list, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 's':
+					len += print_string(list);
+					break;
+				default:
+					len += write(1, format, 1);
+					break;
+			}
+		}
+		else
+		{
+			len += write(1, format, 1);
+		}
+		format++;
+	}
+
+	va_end(list);
+
+	return (len);
+}
